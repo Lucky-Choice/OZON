ec1c3d046d46c477a69f6a299aed040e6bb4d165
Max <erosspd@gmail.com>
2021-10-31 17:38:42 +0300

    NEW @classmethod>>> setUpClass & tearDownClass

--- OZON_test_1.py
+++ OZON_test_1.py
@@ -4,16 +4,31 @@ import unittest
 # тестовую ф-ю def test_first внутри класса MyTest которая в качестве аргумента принимает self
 # self.assertEqual Функция, которая в ответет выводит не равное значение
 # test_second вывод конкретной функции через папки python3 -m unittest OZON_test_1.MyTest.test_second -v
-# setUp - перед запуском теста выводит слово  start
-class MyTest(unittest.TestCase):
+# setUp - перед запуском теста выводит слово  start test
+# tearDown - после теста выводит слово  finish
+# @classmethod - обявили метод класса
+class MyTest(unittest.TestCase): 
+    @classmethod
+    def setUpClass(cls):
+        print("Before all")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("After all")    
+
     def setUp(self):
         print("start test")
 
+    def tearDown(self):
+        print("finish")
+
     def test_first(self):
+        print("one")
         sum_ = 2+2
         assert sum_ == 4
 
     def test_second(self):
+        print("tow")
         sum_ = 2 + 2
         assert sum_ == 4    
 
