237513fd5ef1f350e52cbe18842e9087a497e07b
Max <erosspd@gmail.com>
2021-10-31 16:49:51 +0300

    output cgtwbabr function test_second вывод конкретной функции через папки python3 -m unittest OZON_test_1.MyTest.test_second -v

--- OZON_test_1.py
+++ OZON_test_1.py
@@ -2,10 +2,16 @@ import unittest
 
 # класс MyTest унаследован от unittest.TestCase 
 # тестовую ф-ю def test_first внутри класса MyTest которая в качестве аргумента принимает self
+# self.assertEqual Функция, которая в ответет выводит не равное значение
+# test_second вывод конкретной функции через папки python3 -m unittest OZON_test_1.MyTest.test_second -v
 class MyTest(unittest.TestCase):
     def test_first(self):
         sum_ = 2+2
-        self.assertEqual(sum_, 7)
+        assert sum_ == 4
+
+    def test_second(self):
+        sum_ = 2 + 2
+        assert sum_ == 4    
 
 
 if __name__ == '__main__':
